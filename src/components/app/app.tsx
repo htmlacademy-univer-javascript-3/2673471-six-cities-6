e23d4f58f692c15cdabe9d26564d88aad45860ce
ILYA <ylmaskylov-ilia@mail.ru>
2025-09-23 23:10:53 +0500

    task 2 done

---
+++ src/components/app/app.tsx
@@ -0,0 +1,10 @@
+﻿import Main from '../../pages/main/main.tsx';
+
+type AppProps = {
+  offersCount : number;
+}
+export default function App({offersCount} : AppProps){
+  return (
+    <Main offersCount={offersCount} />
+  );
+}
